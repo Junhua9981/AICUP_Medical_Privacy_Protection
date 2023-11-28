e313faf75e9a24c1d084cbed805942e340415945
Junhua9981 <lichu9981@gmail.com>
2023-11-29 03:23:23 +0800

    f**k this fix time logic bug still lost something

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def main():
     for t in times:
         # parse_type = -1
 
-        # try:
+        try:
             if t.type in parser.parsers.keys():
                 ans = parser.parse(t.text, t.type)
             else:
@@ -62,23 +62,23 @@ def main():
                 # print_log(t, ans)
 
                 
-        # except Exception as e:
-        #     error_class = e.__class__.__name__ #取得錯誤類型
-        #     detail = e.args[0] #取得詳細內容
-        #     cl, exc, tb = sys.exc_info() #取得Call Stack
-        #     lastCallStack = traceback.extract_tb(tb)[-1] #取得Call Stack的最後一筆資料
-        #     fileName = lastCallStack[0] #取得發生的檔案名稱
-        #     lineNum = lastCallStack[1] #取得發生的行號
-        #     funcName = lastCallStack[2] #取得發生的函數名稱
-        #     errMsg = "File \"{}\", line {}, in {}: [{}] {}".format(fileName, lineNum, funcName, error_class, detail)
-        #     print(errMsg)
-        #     print(f"{t.text} \t {t.answer} \t {parse_type}")
-        #     print()
-        #     error += 1
+        except Exception as e:
+            error_class = e.__class__.__name__ #取得錯誤類型
+            detail = e.args[0] #取得詳細內容
+            cl, exc, tb = sys.exc_info() #取得Call Stack
+            lastCallStack = traceback.extract_tb(tb)[-1] #取得Call Stack的最後一筆資料
+            fileName = lastCallStack[0] #取得發生的檔案名稱
+            lineNum = lastCallStack[1] #取得發生的行號
+            funcName = lastCallStack[2] #取得發生的函數名稱
+            errMsg = "File \"{}\", line {}, in {}: [{}] {}".format(fileName, lineNum, funcName, error_class, detail)
+            print(errMsg)
+            # print(f"{t.text} \t {t.answer} \t {parse_type}")
+            print()
+            error += 1
 
-        #     print(f'{t.text} {t.answer}  ERROR')
-        # finally:
-        #     pass
+            print(f'{t.text} {t.answer}  ERROR')
+        finally:
+            pass
 
     print(f'Correct: {correct}, Wrong: {wrong}, Error: {error}, Swaped: {swaped_md}')
     print(f'Accuracy: {correct/(correct+wrong+error) * 100}%')
--- parsers/time_parser.py
+++ parsers/time_parser.py
@@ -5,87 +5,153 @@ import re
 
 class TimeParser(BasicParser):
     def parse(self, text):
+        # print(text)
         text = text.lower()
         has_sec = False
+        if 'at' in text or 'on' in text or '@' in text:
+            if 'at' in text:
+                text = text.replace('fcs1', '')
+                text = text.replace('insrsid8013677', '')
+                date, time = text.split('at')
+            elif 'on' in text:
+                time, date = text.split('on')
+            elif '@' in text:
+                time, date = text.split('@')
+        
+            if 'am' in date or 'pm' in date:
+                date, time = time, date
 
-        if 'at' in text:
-            date, time = text.split('at')
-            if len(re.findall(r'\d+', time)) == 2:
-                date = date.replace('th', '').replace('st', '').replace('nd', '').replace('rd', '').replace('of', '')
-                date = date.split(' ')
-                if len(date) == 3:
-                    for d in date: d = d.strip()
-                    d[1] = d[1][:3]
-                    date = '-'.join(d)
-                else:
-                    date = '-'.join(date)
-            date = date.replace(',', '-').replace('.', '-').replace('/', '-')
             try:
                 date = dateutil.parser.parse(date)
             except:
-                raise ValueError(f'Cannot parse date {date}')
+                date = TimeParser.extract_date(date)
 
-            if len(re.findall(r'\d+', time)) == 3:
+            if len(re.findall(r'\d+', time)) == 3 or len(re.findall(r'\d+', time)) == 6:
                 has_sec = True
             
-            time = TimeParser.extrac_time(time)
+            time, has_sec = TimeParser.extrac_time(time)
 
-        elif 'on' in text:
-            time, date = text.split('on')
+        # elif 'on' in text:
+        #     time, date = text.split('on')
             
-            if 'am' in date or 'pm' in date:
-                date, time = time, date
+        #     if 'am' in date or 'pm' in date:
+        #         date, time = time, date
             
-            time = TimeParser.extrac_time(time)
+        #     time = TimeParser.extrac_time(time)
 
-            date.replace(',', '-').replace('.', '-')
-            if date.count('-') == 2:
-                d, m, y = map(int, date.split('-'))
-                if m > 12 : m, d = d, m
-                if y < 1000 : y = 2000 + y
-                date = datetime(y, m, d)
-            elif 'of' in date:  ##the 25th of September 2013
-                date = date.replace('of', '').replace('th', '').replace('st', '').replace('nd', '').replace('rd', '')
-                date = dateutil.parser.parse(date)
-            else:
-                date = dateutil.parser.parse(date)
+        #     date.replace(',', '-').replace('.', '-')
+        #     if date.count('-') == 2:
+        #         d, m, y = map(int, date.split('-'))
+        #         if m > 12 : m, d = d, m
+        #         if y < 1000 : y = 2000 + y
+        #         date = datetime(y, m, d)
+        #     elif 'of' in date:  ##the 25th of September 2013
+        #         date = date.replace('of', '').replace('th', '').replace('st', '').replace('nd', '').replace('rd', '')
+        #         date = dateutil.parser.parse(date)
+        #     else:
+        #         date = dateutil.parser.parse(date)
             
             
-        elif '@' in text:
-            time, date = text.split('@')
-            if len(re.findall(r'\d+', time)) == 3: ## time 可能會有秒 所以這邊錯
-                time, date = date, time
+        # elif '@' in text:
+        #     time, date = text.split('@')
+        #     if len(re.findall(r'\d+', time)) == 3: ## time 可能會有秒 所以這邊錯
+        #         time, date = date, time
 
-            time = TimeParser.extrac_time(time)
+        #     time = TimeParser.extrac_time(time)
 
-            date.replace(',', '-').replace('.', '-')
-            date = dateutil.parser.parse(date)
+        #     date.replace(',', '-').replace('.', '-')
+        #     date = dateutil.parser.parse(date)
         
         elif len(re.findall(r'\d+', text)) == 6:
+
             has_sec = True
             date = re.findall(r'\d+', text)
             if len(re.findall(r'\d+', text)[0])==4:
                 date = datetime(int(date[0]), int(date[1]), int(date[2]), int(date[3]), int(date[4]), int(date[5]))
-                time = datetime(1,1,1, date.hour, date.minute, date.second)
+                time = date
+            
+            elif len(re.findall(r'\d+', text)[2])==4:
+                date = datetime(int(date[2]), int(date[1]), int(date[0]), int(date[3]), int(date[4]), int(date[5]))
+                time = date
+
+            # date = re.findall(r'\d+', text)
+            # if len(re.findall(r'\d+', text)[0])==4:
+            #     date = datetime(int(date[0]), int(date[1]), int(date[2]), int(date[3]), int(date[4]), int(date[5]))
+            #     time = datetime(1,1,1, date.hour, date.minute, date.second)
+
+            # else:
+            #     print(date)
+            #     date = datetime(int(date[3]), int(date[4]), int(date[5]), int(date[0]), int(date[1]), int(date[2]))
+            #     time = datetime(int(date[3]), int(date[4]), int(date[5]), int(date[0]), int(date[1]), int(date[2]))
+        
+        elif len(re.findall(r'\d+', text)) == 5:
+            date = re.findall(r'\d+', text)
+            # print(date)
+            if len(re.findall(r'\d+', text)[0])==4:
+                date = datetime(int(date[0]), int(date[1]), int(date[2]), int(date[3]), int(date[4]))
+                time = date
+            
+            elif len(re.findall(r'\d+', text)[2])==4:
+                date = datetime(int(date[2]), int(date[1]), int(date[0]), int(date[3]), int(date[4]))
+                time = date
 
             else:
-                date = datetime(int(date[3]), int(date[4]), int(date[5]), int(date[0]), int(date[1]), int(date[2]))
-                time = datetime(int(date[3]), int(date[4]), int(date[5]), int(date[0]), int(date[1]), int(date[2]))
-
-        
+                if 'pm' in text and text.index('pm') < len(text) / 2 +3:
+                    date = datetime(int(date[4]), int(date[3]), int(date[2]), int(date[0])+(12 if int(date[0])<12 else 0), int(date[1].replace('pm', '')))
+                    time = date
+                elif 'am' in text and text.index('am') < len(text) / 2 +3:
+                    date = datetime(int(date[4]), int(date[3]), int(date[2]), int(date[0]), int(date[1].replace('am', '')))
+                    time = date
+                elif 'pm' in text :
+                    date = datetime(int(date[2]), int(date[1]), int(date[0])+(12 if int(date[0])<12 else 0), int(date[3]), int(date[4]))
+                    time = date
+                elif 'am' in text :
+                    date = datetime(int(date[2]), int(date[1]), int(date[0]), int(date[3]), int(date[4]))
+                    time = date
+                else:
+                    date = datetime(int(date[4]), int(date[3]), int(date[2]), int(date[0]), int(date[1]))
+                    time = date
         else:
-            date = dateutil.parser.parse(text)
-            time = datetime(date.year, date.month, date.day, 0, 0, 0)
+            return None, None, None
+        try:
+            if has_sec:
+                return datetime.strftime(datetime(date.year, date.month, date.day, time.hour, time.minute, time.second), "%Y-%m-%dT%H:%M:%S")
+            return datetime.strftime(datetime(date.year, date.month, date.day, time.hour, time.minute, time.second), "%Y-%m-%dT%H:%M")
+        except:
+            return None, None, None
 
-        if has_sec:
-            return datetime.strftime(datetime(date.year, date.month, date.day, time.hour, time.minute, time.second), "%Y-%m-%dT%H:%M:%S")
-        return datetime.strftime(datetime(date.year, date.month, date.day, time.hour, time.minute, time.second), "%Y-%m-%dT%H:%M")
+    @staticmethod
+    def extract_date(txt):
+        txt = txt.lower()
+        txt = txt.replace(',', '-').replace('.', '-').replace('/', '-')
+        if len( re.findall(r'\d+', txt) ) == 3:
+            d, m, y = map(int, re.findall(r'\d+', txt))
+            if m > 12 and d >31: 
+                m = m%12
+                d = d%31
 
+            if m > 12 : m, d = d, m
+            if y < 1000 : y = 2000 + y
+            return datetime(y, m, d)
+        elif 'of' in txt:
+            month_dict = {
+                'january': 1, 'february': 2, 'march': 3, 'april': 4,
+                'may': 5, 'june': 6, 'july': 7, 'august': 8,
+                'september': 9, 'october': 10, 'november': 11, 'december': 12
+            }
+            for month in month_dict.keys():
+                if month in txt:
+                    m = month_dict[month]
+                    break
+            d = int(re.findall(r'\d+', txt)[0])
+            y = int(re.findall(r'\d+', txt)[1])
+            return datetime(y, m, d)
+            
 
     @staticmethod
     def extrac_time(txt):
         txt = txt.lower()
-
+        has_sec = False
         eps = 0
 
         if 'am' in txt:
@@ -96,21 +162,30 @@ class TimeParser(BasicParser):
 
         txt = txt.replace('.', '').replace(',', '').replace(':', '')
         time = re.findall(r'\d+', txt)[0]
-        if len(time) == 5: ##hmmss
+        if len(time) == 5:   # hmmss
             time = '0' + time
             time = datetime(1,1,1, int(time[:2]), int(time[2:4]), int(time[4:6]))
-        elif len(time) == 6:
+            has_sec = True
+        elif len(time) == 6: # hhmmss
             time = datetime(1,1,1, int(time[:2]), int(time[2:4]), int(time[4:6]))
-        elif len(time) == 4:
+            has_sec = True
+        elif len(time) == 4: # hhmm
             time = datetime(1,1,1, int(time[:2]), int(time[2:4]))
         elif len(time) == 3:
             time = datetime(1,1,1, int(time[:1]), int(time[1:3]))
+        elif len(time) == 2:
+            time = datetime(1,1,1, int(time[:2]), 0)
+        elif len(time) == 1:
+            time = datetime(1,1,1, int(time[:1]), 0)
         else:
             time = datetime(1,1,1, int(time[:2]), int(time[2:4]))
         
         if eps != 0 and time.hour == 12:
             eps = 0
-        time = time.replace(hour=time.hour+eps)
-        return time
+        try:
+            time = time.replace(hour=time.hour+eps)
+        except:
+            time = time.replace(hour=time.hour)
+        return time, has_sec
 
         
